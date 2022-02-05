dfcab723e9b138ed1fa2c5f72adbd1a545a7e1c9
João PV Correia <correia.jpv@gmail.com>
2022-02-05 19:37:10 +0000

    Revert "🐛 Bugfix"
    This reverts commit fab630d7d962cbb6358830bcb66ecc3683e443c4.

--- sync_repo.sh
+++ sync_repo.sh
@@ -1,16 +1,14 @@
 #!/bin/sh -l
 
 set -e
-set -x
 
 SOURCE_REPO=$1
-SOURCE_BRANCH=$2
+SOURCE_BRANCH=HEAD
 DESTINATION_REPO=$3
 DESTINATION_BRANCH=$4
-IGNORE_FOLDER=$5
 
-git config --global user.name "$GITHUB_ACTOR"
-git config --global user.email "$GITHUB_ACTOR@users.noreply.github.com"
+git config user.name "$GITHUB_ACTOR"
+git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
 
 SOURCE_REPO="https://x-access-token:$SECRET_TOKEN@github.com/${SOURCE_REPO}.git"
 DESTINATION_REPO="https://x-access-token:$SECRET_TOKEN@github.com/${DESTINATION_REPO}.git"
@@ -20,6 +18,11 @@ echo "DESTINATION=$DESTINATION_REPO:$DESTINATION_BRANCH"
 
 git clone "$SOURCE_REPO" /root/source --origin source && cd /root/source
 
+if [ -z "$INPUT_IGNORE_FOLDER" ]
+then
+	git rm -r $INPUT_IGNORE_FOLDER
+	git commit . -m "Remove $INPUT_IGNORE_FOLDER"
+fi
 
 git remote add destination "$DESTINATION_REPO"
 
@@ -29,11 +32,5 @@ git fetch source '+refs/heads/*:refs/heads/*' --update-head-ok
 # Print out all branches
 git --no-pager branch -a -vv
 
-git push destination "${SOURCE_BRANCH}:${DESTINATION_BRANCH}" -f
 
-if [ ! -z "$IGNORE_FOLDER" ]
-then
-	git rm -rf $IGNORE_FOLDER ||:
-	git commit . -m "Remove $IGNORE_FOLDER"
-	git push
-fi
+git push destination "${SOURCE_BRANCH}:${DESTINATION_BRANCH}" -f
